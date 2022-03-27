e9ae8a378f063fa870764aec7c7454c61d1e30fd
kant <kant@DESKTOP-JOH00M9>
2022-03-27 16:19:49 +0300

    Добавьте файлы проекта.

---
+++ integer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace integer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int namber = -10;
+            Console.WriteLine(namber);
+            namber = 10;
+            Console.WriteLine(namber);
+            namber = 5;
+            Console.WriteLine(namber);
+        }
+    }
+}
